55c6401ec5399a73d9844d6f480aa439e2fbdc1a
“sougata143” <sougata.rintu@gmail.com>
2024-11-28 13:24:58 +0530

    fix: remove icon and splash configuration temporarily
    Removed icon and splash configuration to fix prebuild issues.
    Will add them back once we have proper assets.

--- app.config.js
+++ app.config.js
@@ -4,13 +4,7 @@ module.exports = {
     slug: "thunder-control",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icon.png",
     userInterfaceStyle: "light",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
     assetBundlePatterns: [
       "**/*"
     ],
@@ -25,10 +19,6 @@ module.exports = {
       }
     },
     android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      },
       package: "com.yourcompany.thundercontrol",
       googleServicesFile: "./google-services.json",
       permissions: ["INTERNET"]
